118a58081de970419cc7a1f08c11a761027b5a61
swastia <swasti.arya02gmail.com>
2022-08-31 22:30:12 +0530

    Valid Anagram

--- src/com/neet/easy/TwoSum_1.java
+++ src/com/neet/easy/TwoSum_1.java
@@ -1,4 +1,4 @@
-package com.neet;
+package com.neet.easy;
 
 import java.util.Arrays;
 import java.util.HashMap;
---
+++ src/com/neet/easy/ValidAnagram_242.java
@@ -0,0 +1,76 @@
+package com.neet.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram_242 {
+
+    // first attempt - my solution
+    // this is only 30% better - will try with HashMap approach and string comparison approach
+    public boolean isAnagram(String s, String t) {
+
+        // check size - if different, return false
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        //sort both arrays and compare
+        char[] sortedS = s.toCharArray();
+        Arrays.sort(sortedS);
+
+        char[] sortedT = t.toCharArray();
+        Arrays.sort(sortedT);
+
+        int i = 0;
+        while(i < sortedS.length){
+            if(sortedS[i] != sortedT[i]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+
+
+    /*
+    * Runtime: 10 ms, faster than 35.95% of Java online submissions for Valid Anagram.
+    Memory Usage: 45.1 MB, less than 31.58% of Java online submissions for Valid Anagram.
+* */
+
+    public boolean isAnagramStringCompare(String s, String t){
+        // check size - if different, return false
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        //sort both arrays and compare
+        char[] sortedS = s.toCharArray();
+        Arrays.sort(sortedS);
+        s = new String(sortedS);
+
+        char[] sortedT = t.toCharArray();
+        Arrays.sort(sortedT);
+        t = new String(sortedT);
+
+        if(s.equals(t)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        ValidAnagram_242 anagram = new ValidAnagram_242();
+        System.out.println(anagram.isAnagram("anagram", "nagaram"));
+        System.out.println(anagram.isAnagram("swasti", "Swasti"));
+        System.out.println(anagram.isAnagram("cat", "rat"));
+
+        System.out.println("\n");
+
+        System.out.println(anagram.isAnagramStringCompare("anagram", "nagaram"));
+        System.out.println(anagram.isAnagramStringCompare("swasti", "Swasti"));
+        System.out.println(anagram.isAnagramStringCompare("cat", "rat"));
+
+    }
+}
