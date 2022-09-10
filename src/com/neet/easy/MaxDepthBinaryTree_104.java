3baa52d7318439188c9a6974290e298058b13fa8
Swasti Arya <swasti.arya.eb@gmail.com>
2022-09-10 07:18:39 +0530

    Maximum depth of Binary Tree - Improved version added

--- src/com/neet/easy/MaxDepthBinaryTree_104.java
+++ src/com/neet/easy/MaxDepthBinaryTree_104.java
@@ -46,4 +46,12 @@ Memory Usage: 42.9 MB, less than 65.26% of Java online submissions for Maximum D
         return  Math.max(findMaxDepth(node.left,height), findMaxDepth(node.right,height));
 
     }
+
+    // Below is another improved version of above code, with less time complexity
+    // we are still using Recursive DFS, but there is no need to pass the height or count around.
+    // function should always return 1 + (max(L, R) height of rest of the sub-tree)
+    public int maxDepth2(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
 }
